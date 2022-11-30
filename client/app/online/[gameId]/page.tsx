4640933601e416008093f583f1df88c46ef61833
Lucas Losekann <lucas1losekann@gmail.com>
2022-11-30 16:49:53 +0000

    Implemented board, game logic, moves and winner checking

--- client/app/online/[gameId]/page.tsx
+++ client/app/online/[gameId]/page.tsx
@@ -1,9 +1,22 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
-import { io, Socket} from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
+import styles from './gamepage.module.css';
 
 let socket: Socket;
+interface Room {
+  players: {
+    [id: string]: {
+      timestamp: string;
+      name: string;
+      isX: boolean;
+    };
+  }
+  started: boolean;
+  table: string[][];
+  xTurn: boolean
+}
 
 const onlineGame = ({ params }: {
   params: {
@@ -13,26 +26,47 @@ const onlineGame = ({ params }: {
   const router = useRouter()
   const [isConnected, setIsConnected] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
+  const [table, setTable] = useState([] as string[][]);
+  const [isX, setIsX] = useState(false);
+  const [xTurn, setXTurn] = useState(true);
+
+  const handlePlay = (row: number, cell: number) => {
+    if (!socket) return;
+    if (!gameStarted) return;
+    if (isX != xTurn) return
+    if (table[row][cell] != "") return;
+    socket.emit('makeMove', { row, cell });
+  }
+
+
   useEffect(() => {
 
-    if(!localStorage.getItem('name')){
+    if (!localStorage.getItem('name')) {
       router.back();
       return
     }
-    if(params.gameId.length != 5){
+    if (params.gameId.length != 5) {
       router.back();
       return
     }
 
 
-    socket = io('http://168.138.143.251:3002',{
+    socket = io('http://168.138.143.251:3002', {
       query: {
         roomid: params.gameId,
         userName: localStorage.getItem('name')
       }
     });
-    socket.on('gameStart', ()=>{
+    socket.on('gameStart', (room: Room) => {
+      setTable(room.table);
+      setXTurn(room.xTurn);
+      setIsX(room.players[socket.id].isX);
       setGameStarted(true)
+    })
+    socket.on('gameUpdate', (room: Room) => {
+      setTable(room.table);
+      setXTurn(room.xTurn);
+      setGameStarted(room.started);
     })
 
     socket.on('connect', () => {
@@ -43,7 +77,7 @@ const onlineGame = ({ params }: {
     socket.on('disconnect', () => {
       router.back();
     });
-    
+
 
 
     return () => {
@@ -55,11 +89,22 @@ const onlineGame = ({ params }: {
   }, []);
 
 
-  return <div>
+  return <div className={styles.container}>
     {isConnected ? (
       gameStarted ? (
-        <div>TABULEIRO</div>
-      ): (
+        <>
+          <h1>{xTurn != isX ? "Oponente jogando" : "Sua vez de jogar"}</h1>
+          <div className={styles.gameTable}>
+            {table.map((row, idx1) => {
+              return <div className={styles.row} key={'ROW' + idx1}>
+                {row.map((cell, idx2) => {
+                  return <div onClick={() => handlePlay(idx1, idx2)} key={'CELL' + idx2 + idx1} className={styles.cell}>{cell}</div>
+                })}
+              </div>
+            })}
+          </div>
+        </>
+      ) : (
         <div>Esperando por outros jogadores</div>
       )
     ) : (
--- websocket/index.ts
+++ websocket/index.ts
@@ -1,9 +1,13 @@
 interface ServerToClientEvents {
-  gameStart: () => void;
+  gameStart: (room: Room) => void;
+  gameUpdate: (room: Room) => void;
 }
 
 interface ClientToServerEvents {
-
+  makeMove: (coord: {
+    row: number,
+    cell: number
+  }) => void;
 }
 
 interface InterServerEvents {
@@ -25,14 +29,19 @@ interface Room {
     };
   }
   started: boolean;
-  table: string[];
+  table: string[][];
+  xTurn: boolean;
 }
 interface Rooms {
   [id: string] : Room;
 }
 
 
-const DEFAULT_TABLE : string[] = [];
+const GET_DEFAULT_TABLE : ()=>string[][] = ()=>([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+]);
 
 
 import express from "express";
@@ -87,9 +96,10 @@ io.on('connection', (socket) => {
     console.log(`${socket.data.name}-${socket.id} entrou na sala ${socket.data.room}`);
   }else{
     activeRoom = {
+      xTurn: true,
       players: {},
       started: false,
-      table: DEFAULT_TABLE
+      table: GET_DEFAULT_TABLE()
     }
     activeRoom.players[socket.id] = {
       timestamp : Date.now().toString(),
@@ -104,9 +114,30 @@ io.on('connection', (socket) => {
 
   if((Object.entries(rooms[socket.data.room]?.players ?? {}).length) === 2){
     rooms[socket.data.room].started = true;
-    io.to(socket.data.room).emit('gameStart');
+    rooms[socket.data.room].xTurn = true;
+    io.to(socket.data.room).emit('gameStart', activeRoom);
   }
 
+
+
+  socket.on('makeMove', ({ cell, row })=>{
+    if(!socket.data.room)return;
+    const room = rooms[socket.data.room]
+    const player = room.players[socket.id]
+    if(!player)return;
+    if(room.xTurn != player.isX)return;
+    if(room.table[row][cell] != "")return;
+    room.xTurn = !room.xTurn;
+    room.table[row][cell] = player.isX ? 'X' : 'O';
+    if(checkEndGame([...room.table]).end){
+      room.xTurn = true;
+      room.table = GET_DEFAULT_TABLE();
+      room.started = true;
+      io.to(socket.data.room).emit('gameUpdate', room)
+      return;
+    };
+    io.to(socket.data.room).emit('gameUpdate', room)
+  })
   socket.on('disconnect', ()=>{
     if(socket.data.room){
       socket.leave(socket.data.room);
@@ -114,7 +145,7 @@ io.on('connection', (socket) => {
     delete activeRoom.players[socket.id];
     if(socket.data.room){
       rooms[socket.data.room].started = false;
-      rooms[socket.data.room].table = DEFAULT_TABLE;
+      rooms[socket.data.room].table = GET_DEFAULT_TABLE();
     }
     if(Object.entries(activeRoom.players ?? {}).length == 0){
       if(socket.data.room){
@@ -138,3 +169,90 @@ function getIsX(room: string) : boolean{
     return !playersInRoom[0][1].isX
   }
 }
+
+
+function checkEndGame(table: string[][]) : {end: boolean, xWin?: boolean, tie?: boolean} {
+  let endObj = {
+    end:false,
+    xWin: false,
+    tie: false
+  }
+
+  //Horizontal Checking
+  table.forEach(row => {
+    let X = row.every((cell) => cell == 'X')
+    let O = row.every((cell) => cell == 'O')
+    if(X){
+      endObj =  {
+        end: true,
+        xWin: true,
+        tie: false
+      };
+    }else if(O){
+      endObj =  {
+        end: true,
+        xWin: false,
+        tie: false
+      };
+    }
+  })
+
+  //Vertical Checking
+  for (let i = 0; i < table.length; i++) {
+    const column = [table[0][i],table[1][i],table[2][i]];
+    let X = column.every((cell) => cell == 'X')
+    let O = column.every((cell) => cell == 'O')
+    if(X){
+      endObj =  {
+        end: true,
+        xWin: true,
+        tie: false
+      };
+    }else if(O){
+      endObj =  {
+        end: true,
+        xWin: false,
+        tie: false
+      };
+    }
+  }
+
+
+  //Diagonal Checking
+  if(table[0][0] == 'X' && table[1][1] == 'X' && table[2][2] == 'X'){
+    endObj =  {
+      end: true,
+      xWin: true,
+      tie: false
+    };
+  };
+  if(table[0][0] == 'O' && table[1][1] == 'O' && table[2][2] == 'O'){
+    endObj =  {
+      end: true,
+      xWin: false,
+      tie: false
+    };
+  };
+
+  //Opposite Diagonal Checking
+  if(table[0][2] == 'X' && table[1][1] == 'X' && table[2][0] == 'X'){
+    endObj =  {
+      end: true,
+      xWin: true,
+      tie: false
+    };
+  };
+  if(table[0][2] == 'O' && table[1][1] == 'O' && table[2][0] == 'O'){
+    endObj =  {
+      end: true,
+      xWin: false,
+      tie: false
+    };
+  };
+
+  if(table.every(row => row.every(cell => cell != ""))){
+    endObj.end = true;
+    endObj.tie = true;
+  };
+  return endObj;
+} 
